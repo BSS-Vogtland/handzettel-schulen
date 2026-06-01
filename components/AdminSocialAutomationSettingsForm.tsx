a6bcde9c527a194dd6816a719329ac65f2081a88
Marius Röthig <kontakt@bss-vogtland.de>
2026-06-01 15:49:41 +0200

    Fix SocialPilot automation date formatting

--- components/AdminSocialAutomationSettingsForm.tsx
+++ components/AdminSocialAutomationSettingsForm.tsx
@@ -87,7 +87,9 @@ function toIsoDate(date: Date) {
 function formatDate(date: Date) {
   return new Intl.DateTimeFormat("de-DE", {
     weekday: "long",
-    dateStyle: "medium",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
   }).format(date);
 }
 
