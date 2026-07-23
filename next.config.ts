20483b1615fed0aa546ad563b2c5cad2d7b5d87b
Marius Röthig <kontakt@bss-vogtland.de>
2026-07-23 13:24:49 +0200

    fix: alte Produkt-URLs dauerhaft weiterleiten

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,25 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source:
+          "/shop/produkt/schreibblock-a4-lineatur-7-hs-blk-a4-021",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schreibblock-a4-lineatur-7",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/pinsel-10",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/pinsel-10-borstenpinsel",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/pinsel-sortiment",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/pinselsortiment-pinselset-4-rundpinsel-2-flachpinsel",
+        permanent: true,
+      },
+      {
         source: "/upload",
         destination: "https://www.handzettel-schulen.de/",
         permanent: true,
