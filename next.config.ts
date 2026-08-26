2ca0defc4f465b49b3b73d61985920e39d2557ec
Marius Röthig <kontakt@bss-vogtland.de>
2026-08-26 19:15:30 +0200

    SEO: normalize school notebook slugs and add redirects

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,95 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
+      // Schulheft-Normalisierung
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-0",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-0",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-1-a4",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a4-lineatur-1",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-25-a4",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a4-lineatur-25",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-27-a4",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a4-lineatur-27",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-4-a5",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-4",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-9-a5",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-9",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-lineatur-dm-a4",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a4-lineatur-dm",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a4-lineatur-3",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a4-lineatur-3",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-1",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-1",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-1-2",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-1",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-0-2",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-0-schreiblernheft",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-3",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-3",
+        permanent: true,
+      },
+      {
+        source: "/shop/produkt/schreibheft-a5-lineatur-8f",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-8f",
+        permanent: true,
+      },
+
+      // Alte Search-Console-URL
+      {
+        source: "/shop/produkt/schulheft-kariert-lineatur-f-a5",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/schulheft-a5-lineatur-8f",
+        permanent: true,
+      },
+
+      // Bereits vorhandene Produkt-Redirects
       {
         source:
           "/shop/produkt/schreibblock-a4-lineatur-7-hs-blk-a4-021",
@@ -22,6 +111,14 @@ const nextConfig: NextConfig = {
           "https://www.handzettel-schulen.de/shop/produkt/pinselsortiment-pinselset-4-rundpinsel-2-flachpinsel",
         permanent: true,
       },
+      {
+        source: "/shop/produkt/pinselsortiment",
+        destination:
+          "https://www.handzettel-schulen.de/shop/produkt/pinselsortiment-pinselset-4-rundpinsel-2-flachpinsel",
+        permanent: true,
+      },
+
+      // Bestehende allgemeine Redirects
       {
         source: "/upload",
         destination: "https://www.handzettel-schulen.de/",
